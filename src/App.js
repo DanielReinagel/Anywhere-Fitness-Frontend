783ebbab8e78c5a195cb6b1a6e33c01daf4e0fda
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-10-18 11:12:33 -0700

    TESTING

--- src/App.js
+++ src/App.js
@@ -19,11 +19,11 @@ function App() {
   }, []);
   return (
     <div>
-      {!instructor&&!client && <Link to='/login'>Log In or Sign Up</Link>}
-      {instructor||client && <Link to='/home'>Home</Link>}
-      {instructor && <Link to='/instructorclasses'>Classes I am Teaching</Link>}
-      {client && <Link to='/classes'>All Classes</Link>}
-      {client && <Link to='/clientclasses'>Classes I am Attending</Link>}
+      {!instructor&&!client && <a {/*to='/login'*/}>Log In or Sign Up</a>}
+      {instructor||client && <a {/*to='/home'*/}>Home</a>}
+      {instructor && <a {/*to='/instructorclasses'*/}>Classes I am Teaching</a>}
+      {client && <a {/*to='/classes'*/}>All Classes</a>}
+      {client && <a {/*to='/clientclasses'*/}>Classes I am Attending</a>}
       {instructor && <button>Be a Client</button>}
     </div>
 
