6b027545e24ebeba57614c97e090e0bf10f97181
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-10-21 12:24:48 -0700

    search bar filters the classes properly

--- src/components/ClientClasses.js
+++ src/components/ClientClasses.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import Classes from './Classes';
+import axiosWithAuth from '../functions/axiosWithAuth';
 
 const ClientClasses = () => {
   const [ classes, setClasses ] = useState([]);
 
+  const user_id = localStorage.getItem('user_id');
+
   useEffect(()=>{
-    //axios set classes
+    if(user_id){
+      axiosWithAuth().get();
+    } else {
+      console.log('User Id Not Found in localStorage')
+    }
   }, []);
 
   return (
