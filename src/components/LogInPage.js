943cec140e460c0b2570aa9e761ffdab587a8596
Ryan Howard <ryanghoward@protonmail.com>
2021-10-20 20:14:44 -0700

    Log In page built

--- src/components/LogInPage.js
+++ src/components/LogInPage.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { EntryPage, PageHeader } from '../style';
+import EntryCard from '../components/EntryCard';
+import InputGroup from '../components/InputGroup';
+import Input from '../components/Input';
+import Button from '../components/Button';
 
 const LogInPage = () => {
   return (
-    <>
-      <h1>Login!</h1>
-      <span>
-        Don't have an account?
-        <Link to='/signup'> Sign Up</Link>
-      </span>
-      <br />
-      <span>Go back<Link to='/home'> Home</Link></span>
-    </>
+    <EntryPage>
+      <PageHeader to='/'>ANYWHERE FITNESS</PageHeader>
+      <EntryCard>
+        <h2>Log In</h2>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <InputGroup>
+            <label htmlFor='login-email'>Email Address</label>
+            <Input type='text' placeholder='Email' id='login-email' />
+          </InputGroup>
+          <InputGroup>
+            <label htmlFor='login-password'>Password</label>
+            <Input type='password' placeholder='Password' id='login-password' />
+          </InputGroup>
+          <Button type='submit' full>Log In</Button>
+        </form>
+        <span>
+          Don't have an account?
+          <Link to='/signup'>Sign Up</Link>
+        </span>
+      </EntryCard>
+    </EntryPage>
   );
 }
 
